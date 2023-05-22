79a726b4e64ed6415a5d990677fdbdda85fbbcff
Eugene Pavlov <pavlov88ev@gmail.com>
2023-05-22 09:35:31 +0300

    8kyu.CalculateAverage

---
+++ src/Codewars/8kyu.CalculateAverage.js
@@ -0,0 +1,17 @@
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+function findAverage(array) {
+  if (array === null || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
+}
