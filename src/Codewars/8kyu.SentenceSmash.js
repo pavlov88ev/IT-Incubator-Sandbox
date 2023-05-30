8473445383e1cbbc1863ac32713aca2e3c631343
Eugene Pavlov <pavlov88ev@gmail.com>
2023-05-30 13:31:25 +0300

    8kyu.SentenceSmash

---
+++ src/Codewars/8kyu.SentenceSmash.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+function smash (words) {
+  if (words.length === 0) {
+    return "";
+  }
+  str = "";
+  for (let i = 0; i < words.length - 1; i++) {
+    str += `${words[i]} `; 
+  }
+  return str += words[words.length - 1];
+};
